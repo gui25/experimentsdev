ad6173f71239cd6ae609eae6e818dce921e7886b
Guilherme <gui25ber@gmail.com>
2020-10-24 12:57:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // const path = require('path')
-
+var db = new sqlite3.Database('database.db');
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000
 
 
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req,res) => res.send(
+    db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+        console.log(row.id + ': ' + row.info);
+    })
+  ))
 
 
 
