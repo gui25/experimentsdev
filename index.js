67ca3499ed207485eba4561b7fe22ea38678bcaf
Guilherme <gui25ber@gmail.com>
2020-10-24 10:07:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(8080);
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  
