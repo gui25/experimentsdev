3993c4f78e4626e61417ebbf2b17a236b458a8a8
Guilherme <gui25ber@gmail.com>
2020-10-24 23:59:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ var db = new sqlite3.Database('database.db');
 const app = express()
 const PORT = process.env.PORT || 5000
 
+var http = require('http');
+var fs = require('fs');
 
-let text = '';
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database.db');
+app.get('/', (req,res) => res.send(
 
-db.serialize(function() {
-  
-  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-    text = text + row.id + ': ' + row.info;
-  });
 
-});
+    http.createServer(function (req, res) {
+        //Open a file on the server and return its content:
+        fs.readFile('demofile1.html', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+        });
+      })
 
-db.close();
-
-app.get('/', (req,res) => res.send(text))
+))
 
 
 
