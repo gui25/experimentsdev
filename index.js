d4de3ebf9f4c5f960eefe0af679e28bd3f68d009
Guilherme <gui25ber@gmail.com>
2020-10-26 19:17:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ var db = new sqlite3.Database('database.db');
 const app = express()
 const PORT = process.env.PORT || 5000
 
-var http = require('http');
-var fs = require('fs');
 
-    http.createServer(function (req, res) {
-        //Open a file on the server and return its content:
-        fs.readFile('db/database.db', function(err, data) {
-          res.writeHead(200, {'Content-Type': 'text/db'});
-          res.write(data);
-          return res.end();
-        });
-    }).listen(PORT, () => console.log(`Listening on port ${ PORT }`));
+
+
+app.get('/', (req,res) => res.send('text'))
+
+
+
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
 
   
