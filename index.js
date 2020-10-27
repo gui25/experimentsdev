8bec00fb99bb17fd2f898b647acb845ccf66f128
Guilherme <gui25ber@gmail.com>
2020-10-27 07:53:02 -0300

    up

--- index.js
+++ index.js
@@ -7,26 +7,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db/database.db');
-
-
-
-
-app.get('/rows', function (req, res) {
-    db.serialize(function() {
-        
-        db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-          console.log(row.id + ': ' + row.info);
-        });
-
-      });
-      
-      db.close();
-  });
-
-
-
 app.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
 
   
