cee713959dcc88f1cfd832de4cda53aeadddbbe4
Guilherme <gui25ber@gmail.com>
2020-10-27 09:23:08 -0300

    update

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', function (req, res) { //endereco da requisicao onde e retornado hello world
-    res.send('Hello World')
-})
-
-app.get('/about', (req, res) => {
-    var users = [{
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placekitten.com/300/300'
-    }, {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placekitten.com/400/300'
-    }, {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placekitten.com/500/300'
-    }]
   
-    res.render('views/pages/about', { usuarios: users })
-  })
-
-
-
-app.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
+// With middleware 
+app.use('/', function(req, res, next){ 
+     
+    var options = { 
+        root: path.join(__dirname) 
+    }; 
+      
+    var fileName = 'db/database.db'; 
+    res.sendFile(fileName, options, function (err) { 
+        if (err) { 
+            next(err); 
+        } else { 
+            console.log('Sent:', fileName); 
+            next(); 
+        } 
+    }); 
+}); 
+  
+app.get('/', function(req, res){ 
+    console.log("File Sent") 
+    res.send(); 
+}); 
+  
+app.listen(PORT, function(err){ 
+    if (err) console.log(err); 
+    console.log("Server listening on PORT", PORT); 
+});
 
   
