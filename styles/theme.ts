9b351de8d861196b220ce30ebba26010a2e5b356
Guilherme <gui25ber@gmail.com>
2020-11-17 03:22:42 -0300

    up

--- styles/theme.ts
+++ styles/theme.ts
@@ -17,21 +17,7 @@ const customTheme: DefaultTheme = {
     ...theme.radii,
     sm: '5px',
     md: '8px',
-  },
-  colors: {
-    ...theme.colors,
-    purple: {
-      ...theme.colors.purple,
-      500: '#8257e5',
-    },
-    gray: {
-      ...theme.colors.gray,
-      300: '#e1e1e6',
-      600: '#29292e',
-      700: '#202024',
-      800: '#121214'
-    },
-  },
+  }
 }
 
 export default customTheme;
