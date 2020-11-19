747d2afc598c28b9f27b202629441556f4f8ec1d
Guilherme <gui25ber@gmail.com>
2020-11-19 00:01:53 -0300

    up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import Document, { DocumentProps, Html, Head, Main, NextScript } from 'next/document'
+
+
 
 class MyDocument extends Document<DocumentProps> {
   render(): JSX.Element {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" type="image/png" href="/public/favicon.png" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet" />  
+          
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet" /> 
+          <link rel="shortcut icon" type="image/png" href="https://gui25.github.io/main/favicon.png" /> 
         </Head>
         <body>
           <Main />
