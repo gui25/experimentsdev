7ebe3961045972e8f8fcd94bcae40f5fb8484acf
Guilherme <gui25ber@gmail.com>
2020-11-19 02:53:52 -0300

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,7 +4,7 @@ import { Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui
 const Input: React.FC<ChakraInputProps> = (props) => {
   return (
     <ChakraInput
-      variant="unstyled"
+      variant="outline"
       height="50px"
       backgroundColor="gray.800"
       focusBorderColor="#4d4dff"
