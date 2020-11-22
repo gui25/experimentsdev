474f6191ad54d86b5ac40a7f01a138f4fe906d53
Guilherme <gui25ber@gmail.com>
2020-11-22 17:04:28 -0300

    up

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,27 +1,6 @@
 import { NowRequest, NowResponse } from '@vercel/node'
-import { MongoClient, Db } from 'mongodb'
-import url from 'url';
+import connectToDatabase from '../api/database';
 
-let cachedDb: Db = null;
-
-async function connectToDatabase(uri: string) {
-  if (cachedDb) {
-    return cachedDb;
-  }
-
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const dbName = url.parse(uri).pathname.substr(1);
-
-  const db = client.db(dbName);
-
-  cachedDb = db;
-
-  return db;
-}
 
 export default async (request: NowRequest, response: NowResponse) => {
   const { email } = request.body;
--- pages/api/subshow.ts
+++ pages/api/subshow.ts
@@ -1,33 +1,15 @@
 import { NowRequest, NowResponse } from '@vercel/node'
-import { MongoClient, Db } from 'mongodb'
-import url from 'url';
+import connectToDatabase from '../api/database';
 
-let cachedDb: Db = null;
 
-async function connectToDatabase(uri: string) {
-  if (cachedDb) {
-    return cachedDb;
-  }
-
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const dbName = url.parse(uri).pathname.substr(1);
-
-  const db = client.db(dbName);
-
-  cachedDb = db;
-
-  return db;
-}
-
-export default async () => {
+export default async (request: NowRequest, response: NowResponse) => {
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
 
   const collection = db.collection('subscribers');
 
-  return collection.find();
-  }
+  await collection.find();
+
+
+  return response.status(201).json({ ok: true });
+}
