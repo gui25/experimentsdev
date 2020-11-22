ab91ec4f110b14b9e2c8ac44bdad78468a0f0031
Guilherme <gui25ber@gmail.com>
2020-11-22 03:23:12 -0300

    up

--- pages/extra/showsubs.tsx
+++ pages/extra/showsubs.tsx
@@ -1,8 +1,18 @@
 import { useState, FormEvent } from 'react';
 import { Flex,  Button, Text } from '@chakra-ui/core'
 
-import axios from 'axios';
-
-export default function Home() {
-    axios.get('/api/showsubscribe');
-}
+const axios = require('axios');
+ 
+// Make a request for a user with a given ID
+axios.get('/api/subscribe')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
